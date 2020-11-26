83bdb0537b5b02bd0d1d722bd1e3f4fbc102a382
Raphael Arantes <raphael_arantes26@yahoo.com.br>
2020-11-26 12:34:19 -0300

    config mongo heroku

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/nodedata',  { 
+mongoose.connect('mongodb+srv://admito:m1234567@clus-card-invest-gor.dalez.mongodb.net/<nodedatadb>?retryWrites=true&w=majority', {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useCreateIndex: true,
