e1c4900c40003f81ca29058b04da76dc392827f5
Raphael <raphael.m.arantes@accenture.com>
2020-11-26 13:44:03 -0300

    change lowercase word

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// require('./controllers/auth-controller')(app);
+require('./controllers/auth-controller')(app);
 // require('./controllers/card-invest-controller')(app);
 
 let port = process.env.PORT || 3000;    
